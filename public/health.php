c027d465e903483a56afc38f653b8db1b9566deb
Sebastian Quesada <squesadagonzalez@gmail.com>
2026-03-03 20:06:55 -0600

    health db check

--- public/health.php
+++ public/health.php
@@ -1,3 +1,23 @@
 <?php
-header("Content-Type: text/plain");
-echo "OK HEALTH";
+
+$host = getenv("MYSQL_ADDON_HOST");
+$user = getenv("MYSQL_ADDON_USER");
+$db   = getenv("MYSQL_ADDON_DB");
+
+echo "Servidor de base de datos: " . $host . "<br>";
+echo "Base de datos: " . $db . "<br>";
+
+$conn = new mysqli(
+    getenv("MYSQL_ADDON_HOST"),
+    getenv("MYSQL_ADDON_USER"),
+    getenv("MYSQL_ADDON_PASSWORD"),
+    getenv("MYSQL_ADDON_DB"),
+    getenv("MYSQL_ADDON_PORT")
+);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+echo "Conexion a la base de datos exitosa";
+?>
